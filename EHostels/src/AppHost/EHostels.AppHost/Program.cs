994b8de6d66bda2fcdc8d5243327d7935a8daf48
Ganesh Vijigiri <129714231+GaneshVijigiri@users.noreply.github.com>
2025-05-07 09:00:23 +0530

    Migrated from React to angular

--- EHostels/src/AppHost/EHostels.AppHost/Program.cs
+++ EHostels/src/AppHost/EHostels.AppHost/Program.cs
@@ -5,6 +5,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var eHostelsApi = builder.AddProject<EHostels_API>("EHostels-API");
 var bff = builder.AddProject<EHostels_BFF>("EHostels-BFF").WithReference(eHostelsApi);
-builder.AddNpmApp("frontend", "../../frontend/ehostels.ui", "dev").WithExternalHttpEndpoints();
+builder.AddNpmApp("frontend", "../../frontend/ehostels.ui", "start").WithExternalHttpEndpoints();
 
 builder.Build().Run();
